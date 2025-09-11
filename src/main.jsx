a1865e91b2cfff798359487940c5ca83b368d5d3
Lalithyaliki <ananthaneniliki25@gmail.com>
2025-09-11 20:08:58 +0530

    added js

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { HashRouter } from "react-router-dom";
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
