14183e8340b498d00193a9a5e6bdd41be95e91e8
Lalithyaliki <ananthaneniliki25@gmail.com>
2025-09-12 10:57:22 +0530

    removed carousel

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -153,15 +153,15 @@ function MainSection() {
                             </div>
                         </div>
                     </div>
-
-                    <button className="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
+                    
+                    {/* <button className="carousel-control-prev" type="button" data-bs-target="#customCarousel" data-bs-slide="prev">
                         <span className="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Previous</span>
                     </button>
                     <button className="carousel-control-next" type="button" data-bs-target="#customCarousel" data-bs-slide="next">
                         <span className="carousel-control-next-icon" aria-hidden="true"></span>
                         <span className="visually-hidden">Next</span>
-                    </button>
+                    </button> */}
                 </div>
             </motion.div>
 
